e28d48120c7895d567dd3735d32173d876272c65
Casey Jumu <casey.jumu@sapiosciences.com>
2023-12-26 10:03:31 +0000

    tentth commit

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,24 +1,46 @@
+
+// AppDelegate.swift
 import UIKit
-import Flutter
+import FacebookCore
 import flutter_local_notifications
 
+
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
 
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    // This is required to make any communication available in the action isolate.
-    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
-        GeneratedPluginRegistrant.register(with: registry)
+            FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+                GeneratedPluginRegistrant.register(with: registry)
+            }
+
+            if #available(iOS 10.0, *) {
+              UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+            }
+
+            GeneratedPluginRegistrant.register(with: self)
+
+
+        ApplicationDelegate.shared.application(
+            application,
+            didFinishLaunchingWithOptions: launchOptions
+        )
+
+        return true
     }
 
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+    func application(
+        _ app: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+        ApplicationDelegate.shared.application(
+            app,
+            open: url,
+            sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+            annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+        )
     }
-
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
 }
