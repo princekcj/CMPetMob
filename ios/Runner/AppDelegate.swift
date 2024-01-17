2d1bba8e59f5354bddb8ca0ef79ac540c6d8c424
Casey Jumu <casey.jumu@sapiosciences.com>
2024-01-17 16:24:57 +0000

    beta wrk

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -14,10 +14,6 @@ import flutter_local_notifications
         GeneratedPluginRegistrant.register(with: registry)
     }
 
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
-    }
-
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
