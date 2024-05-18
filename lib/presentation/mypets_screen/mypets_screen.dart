1c355b94032cad7bc8ec835ce6e2a48185426cf9
KC <61117146+princekcj@users.noreply.github.com>
2024-05-18 22:42:47 +0100

    Update mypets_screen.dart

--- lib/presentation/mypets_screen/mypets_screen.dart
+++ lib/presentation/mypets_screen/mypets_screen.dart
@@ -22,11 +22,10 @@ class MyPetsScreen extends StatefulWidget {
 
 class _MyPetsScreenState extends State<MyPetsScreen> {
   bool isMenuOpen = false;
-  // Image cache for precaching
-  final ImageCache _imageCache = ImageCache();
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   User? _currentUser;
+  Map<String, bool> showDeleteButtons = {};
 
   void toggleMenu() {
     setState(() {
@@ -34,150 +33,153 @@ class _MyPetsScreenState extends State<MyPetsScreen> {
     });
   }
 
-  ImageProvider _getImageProvider(String imagePath) {
-    if (imagePath.startsWith("https")) {
-      return NetworkImage(imagePath);
-    } else {
-      return AssetImage(imagePath);
-    }
-  }
-
-
-
-  // Fetch the current user when the screen initializes
   @override
   void initState() {
     super.initState();
     _currentUser = _auth.currentUser;
-
   }
 
+  Future<void> deletePet(String petId) async {
+    if (_currentUser == null) return;
+    try {
+      await _firestore
+          .collection('users')
+          .doc(_currentUser!.uid)
+          .collection('pets')
+          .doc(petId)
+          .delete();
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Pet deleted successfully'),
+      ));
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Failed to delete pet: $e'),
+      ));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     analytics_utils.logScreenUsageEvent('MyPetsScreen');
     return Scaffold(
-      resizeToAvoidBottomInset: false, // fluter 2.x
+      resizeToAvoidBottomInset: false,
       endDrawer: CustomDrawer(),
-        appBar: top_bar.CustomTopAppBar(
-          Enabled: false,
-          onTapArrowLeft: (context) {
-            Navigator.pop(context);
-          },
-          onMenuPressed: toggleMenu,
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: StreamBuilder<QuerySnapshot>(
-                stream: _firestore
-                    .collection('users')
-                    .doc(_currentUser?.uid)
-                    .collection('pets')
-                    .snapshots(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(child: CircularProgressIndicator(color: Color(0xFF008C8C)));
-                  }
+      appBar: CustomTopAppBar(
+        Enabled: false,
+        onTapArrowLeft: (context) {
+          Navigator.pop(context);
+        },
+        onMenuPressed: toggleMenu,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: _firestore
+                  .collection('users')
+                  .doc(_currentUser?.uid)
+                  .collection('pets')
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator(color: Color(0xFF008C8C)));
+                }
 
-                  if (snapshot.hasError) {
-                    return Text('Error: ${snapshot.error}');
-                  }
+                if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                }
 
-                  if (!snapshot.hasData ||
-                      (snapshot.data?.docs.isEmpty ?? true)) {
-                    return Center(child: Text('No pets available.'));
-                  }
+                if (!snapshot.hasData ||
+                    (snapshot.data?.docs.isEmpty ?? true)) {
+                  return Center(child: Text('No pets available.'));
+                }
 
-                  // Map the documents to PetTabs and display
-                  final petTabs = snapshot.data!.docs.map((doc) {
-                    String petName = doc['name'];
-                    String petId = doc.id;
-                    String petWeight = doc['weight'];
-                    String petType = doc['type'];
-                    String image = doc['image'];
-                    List<String> allergies = List<String>.from(doc['allergies'] ?? []); // Adjusting for 'allergies'
-                    String feedingInstructions = doc['feedingInstructions'] ?? ''; // Adjusting for 'feedingInstructions'
-                    List<String> medications = List<String>.from(doc['medications'] ?? []); // Adjusting for 'medications'
-                    Timestamp? dateOfBirthTimestamp = doc['dateOfBirth'] as Timestamp?;
-                    DateTime? dateOfBirth = dateOfBirthTimestamp != null ? dateOfBirthTimestamp.toDate() : null; // Adjusting for 'dateOfBirth'
-                    String moreInfo = doc['moreInfo'];
-                    bool isNeutered = doc['isNeutered'];
-                    String vetName = doc['vetName'];
-                    String insuranceProvider = doc['insuranceProvider'];
+                final petTabs = snapshot.data!.docs.map((doc) {
+                  String petName = doc['name'];
+                  String petId = doc.id;
+                  String petWeight = doc['weight'];
+                  String petType = doc['type'];
+                  String image = doc['image'];
+                  List<String> allergies = List<String>.from(doc['allergies'] ?? []);
+                  String feedingInstructions = doc['feedingInstructions'] ?? '';
+                  List<String> medications = List<String>.from(doc['medications'] ?? []);
+                  Timestamp? dateOfBirthTimestamp = doc['dateOfBirth'] as Timestamp?;
+                  DateTime? dateOfBirth = dateOfBirthTimestamp != null ? dateOfBirthTimestamp.toDate() : null;
+                  String moreInfo = doc['moreInfo'];
+                  bool isNeutered = doc['isNeutered'];
+                  String vetName = doc['vetName'];
+                  String insuranceProvider = doc['insuranceProvider'];
 
-                    // Convert 'appointments' from Firestore to a list of Appointment objects
-                    List<Appointment> appointments = (doc['appointments'] as List<dynamic>)
-                        .map((appointmentData) => Appointment.fromJson(appointmentData))
-                        .toList();
+                  List<Appointment> appointments = (doc['appointments'] as List<dynamic>)
+                      .map((appointmentData) => Appointment.fromJson(appointmentData))
+                      .toList();
 
-
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) =>
-                              MyPetInfoScreen(petId: petId, petName: petName, image: image, petWeight: petWeight, petType: petType, allergies: allergies, feedingInstructions: feedingInstructions, medications: medications, selectedDate: dateOfBirth, moreInfo: moreInfo, appointments: appointments, isNeutered: isNeutered, vetName: vetName, insuranceProvider: insuranceProvider),
-                        ));
-                      },
-                      child: buildPetTab(petName, petType, image),
-                    );
-                  }).toList();
-
-                  return GridView.builder(
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                    ),
-                    itemCount: petTabs.length,
-                    itemBuilder: (context, index) {
-                      return petTabs[index];
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) =>
+                            MyPetInfoScreen(petId: petId, petName: petName, image: image, petWeight: petWeight, petType: petType, allergies: allergies, feedingInstructions: feedingInstructions, medications: medications, selectedDate: dateOfBirth, moreInfo: moreInfo, appointments: appointments, isNeutered: isNeutered, vetName: vetName, insuranceProvider: insuranceProvider),
+                      ));
                     },
+                    onLongPress: () {
+                      setState(() {
+                        showDeleteButtons[petId] = true;
+                      });
+                    },
+                    child: buildPetTab(petName, petType, image, petId),
                   );
-                },
-              ),
-            ),
-            SizedBox(height: 5),
-            // Reduce the height to bring the plus button closer to the GridView
-            GestureDetector(
-              onTap: () {
-                // Navigate to MyPetInfoScreen without passing a pet name
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => MyPetInfoScreen(),
-                ));
+                }).toList();
+
+                return GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                  ),
+                  itemCount: petTabs.length,
+                  itemBuilder: (context, index) {
+                    return petTabs[index];
+                  },
+                );
               },
-              child: Container(
-                width: 60,
-                height: 60,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                    color: Colors.black, // Border color
-                    width: 2.0, // Border width
-                  ),
+            ),
+          ),
+          SizedBox(height: 5),
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => MyPetInfoScreen(),
+              ));
+            },
+            child: Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.black,
+                  width: 2.0,
                 ),
-                child: Center(
-                  child: Icon(
-                    Icons.add,
-                    color: Colors.grey,
-                    size: 40,
-                  ),
+              ),
+              child: Center(
+                child: Icon(
+                  Icons.add,
+                  color: Colors.grey,
+                  size: 40,
                 ),
               ),
             ),
-            SizedBox(height: 65),
-          ],
-        ),
+          ),
+          SizedBox(height: 65),
+        ],
+      ),
       floatingActionButton: Container(height: 80.0, width:80.0, child: FittedBox(child: FloatingActionButton(
-        backgroundColor: Color(0xFF008C8C), // Set the background color to blue
+        backgroundColor: Color(0xFF008C8C),
         child: Image.asset(ImageConstant.searchbutton, width: 40, height: 40),
         shape: RoundedRectangleBorder(
           side: BorderSide(color: ColorConstant.fromHex('#a3ccff'), width: 2.0),
-          borderRadius:
-          BorderRadius.circular(28.0), // Adjust the border radius as needed
+          borderRadius: BorderRadius.circular(28.0),
         ),
         onPressed: () {
-          // Check if the current route is not already the search route
-
           if (ModalRoute.of(context)!.settings.name != AppRoutes.barcodeScreen) {
             Navigator.pushReplacement(
               context,
@@ -186,7 +188,6 @@ class _MyPetsScreenState extends State<MyPetsScreen> {
               ),
             );
           }
-
         },
       ),),),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
@@ -199,64 +200,80 @@ class _MyPetsScreenState extends State<MyPetsScreen> {
           // Handle button press based on index
         },
       ),
-      );
+    );
   }
 
-  Widget buildPetTab(String label, String _petType, String image) {
-    String _petImage = '';
+  Widget buildPetTab(String label, String petType, String image, String petId) {
+    String petImage = '';
 
     if (image.startsWith("https")) {
-      _petImage = image; // Replace with actual image path
-    } else if (_petType == 'cat') {
-        _petImage = ImageConstant.cat; // Replace with actual cat image path
-    } else if (_petType == 'snake') {
-        _petImage = ImageConstant.snake; // Replace with actual snake image path
-    } else if (_petType == 'parrot') {
-        _petImage = ImageConstant.parrot; // Replace with actual parrot image path
-    } else if (_petType == 'dog') {
-        _petImage = ImageConstant.charles; // Replace with actual parrot image path
-    } else if (_petType == 'guinea_pig') {
-      _petImage = ImageConstant.guinea; // Replace with actual parrot image path
-    } else if (_petType == 'bearded_dragon') {
-      _petImage = ImageConstant.dragon; // Replace with actual parrot image path
-    } else if (_petType == 'hamster') {
-      _petImage = ImageConstant.hamster; // Replace with actual parrot image path
+      petImage = image;
+    } else if (petType == 'cat') {
+      petImage = ImageConstant.cat;
+    } else if (petType == 'snake') {
+      petImage = ImageConstant.snake;
+    } else if (petType == 'parrot') {
+      petImage = ImageConstant.parrot;
+    } else if (petType == 'dog') {
+      petImage = ImageConstant.charles;
+    } else if (petType == 'guinea_pig') {
+      petImage = ImageConstant.guinea;
+    } else if (petType == 'bearded_dragon') {
+      petImage = ImageConstant.dragon;
+    } else if (petType == 'hamster') {
+      petImage = ImageConstant.hamster;
     }
-
 
     return Tab(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
-            width: 90, // Adjust the size as needed
+            width: 90,
             height: 90,
             decoration: BoxDecoration(
               shape: BoxShape.circle,
               border: Border.all(
                 color: Colors.white,
-                width: 2, // Adjust the border thickness as needed
+                width: 2,
               ),
             ),
-              child: ClipOval(
-                clipBehavior: Clip.hardEdge,
-              child: _petImage.startsWith("https")
-                  ? CachedNetworkImage(
-                  imageUrl: _petImage,
-                cacheManager: CustomCacheManager.instance,
-                fit: BoxFit.fill,
-                width: 100,
-                height: 100,
-              )
-                  : Image.asset(
-                _petImage, // Replace with your image asset path
-                fit: BoxFit.cover,
-                width: 100,
-                height: 100,
-              ),
+            child: Stack(
+              children: [
+                ClipOval(
+                  clipBehavior: Clip.hardEdge,
+                  child: petImage.startsWith("https")
+                      ? CachedNetworkImage(
+                    imageUrl: petImage,
+                    cacheManager: CustomCacheManager.instance,
+                    fit: BoxFit.fill,
+                    width: 100,
+                    height: 100,
+                  )
+                      : Image.asset(
+                    petImage,
+                    fit: BoxFit.cover,
+                    width: 100,
+                    height: 100,
+                  ),
+                ),
+                if (showDeleteButtons[petId] == true)
+                  Positioned(
+                    right: 0,
+                    child: IconButton(
+                      icon: Icon(Icons.delete, color: Colors.red),
+                      onPressed: () {
+                        deletePet(petId);
+                        setState(() {
+                          showDeleteButtons.remove(petId);
+                        });
+                      },
+                    ),
+                  ),
+              ],
             ),
           ),
-          SizedBox(height: 8), // Add some space between the image and the label
+          SizedBox(height: 8),
           Text(
             label,
             style: TextStyle(
